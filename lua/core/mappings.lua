ac918e65aa3b4582f8ef3cbeaf5438cad674da57
Piotr Skamruk <piotrs@qwilt.com>
2024-01-23 13:52:34 +0100

    Updata key mappings
    * add mappings descriptions
    * add shift-h/l in norm as buffer swappers

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,12 +6,16 @@ g.maplocalleader = '\\'
 local keymap = vim.keymap
 
 -- remap jj in insert mode to ESC
-keymap.set("i", "jj", "<Esc>")
+keymap.set("i", "jj", "<Esc>", { desc = "Use jj in insert mode as ESC"} )
 
 -- moving visually selected block
-keymap.set("v", "<", "<gv")
-keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv", { desc = "De-indent selected block" })
+keymap.set("v", ">", ">gv", { desc = "Indent selected block" })
 
 -- clipboard simplification
-keymap.set({"n", "v"}, "<leader>y", '"+y')
-keymap.set({"n", "v"}, "<leader>p", '"+p')
+keymap.set({"n", "v"}, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+keymap.set({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+
+-- buffers swapping
+keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
