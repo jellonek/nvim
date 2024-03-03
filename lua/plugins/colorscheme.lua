fa68071c43fcdb240d38e69251353c624cdbf22b
Piotr Skamruk <piotr.skamruk@gmail.com>
2024-03-03 17:43:26 +0100

    Change version of colorsheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,14 @@
+-- return {
+--   "michalbachowski/vim-wombat256mod",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("wombat256mod")
+--   end
+-- }
 return {
-  "michalbachowski/vim-wombat256mod",
+  "dsolstad/vim-wombat256i",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("wombat256mod")
+    vim.cmd.colorscheme("wombat256i")
   end
 }
